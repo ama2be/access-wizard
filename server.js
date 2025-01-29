9aa2e6d7f5de9d95fd2528502e1e03cdc88e9f3d
Ammar Abdallah <ama2be@bosch.com>
2025-01-29 16:01:58 +0100

    test endpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const Prometheus = require('prom-client')
 const express = require('express');
+const oracledb = require('oracledb');
 const http = require('http');
 
 Prometheus.collectDefaultMetrics();
@@ -55,6 +56,109 @@ app.get('/', (req, res) => {
   req.log.info({message: 'Hello from Node.js Starter Application!'});
   res.send('Hello from Node.js Starter Application!');
 });
+
+
+const TEST_SECRET = ""+process.env.username
+const x = Buffer.from(TEST_SECRET, 'base64');
+app.get('/test', (req, res) => {
+  req.log.info({message: `Test back ${x}!`});
+  res.send(`Test back ${x}!`);
+});
+
+// my code
+client_secret = ""+Buffer.from(""+process.env.client_secret, 'base64')
+username      = ""+Buffer.from(""+process.env.username     , 'base64')
+password      = ""+Buffer.from(""+process.env.password     , 'base64')
+
+app.get('/accessToken', (req, res) => {
+  getAccessToken(res);
+});
+
+app.get('/tcodes', (req, res) => {  
+  getDb(req.query.accessRight, res);
+});
+
+async function getDb(accessRight, res){
+
+    oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+
+    const connection = await oracledb.getConnection ({
+       user          : username,
+       password      : password,
+       connectString : "(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=si0exarac02.de.bosch.com)(PORT=38000)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=RLDD01_OPS_2.BOSCH.COM)))"
+    });
+    const result = await connection.execute(
+        `SELECT LOW
+         FROM V_CUSN_AGR_1251
+         WHERE ARG_NAME = :accRight`,
+         [accessRight],
+    );
+
+   //const mypw = "oraclepwd"
+   //const connection = await oracledb.getConnection ({
+   //    user          : "C##RAW_E3",
+   //    password      : mypw,
+   //    connectString : "(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=FREE)))"
+   //});
+   //const result = await connection.execute(
+   //    `SELECT LOW
+   //     FROM V_CUSN_AGR_1251
+   //     WHERE ARG_NAME = :accRight`,
+   //     [accessRight],
+   //);
+
+
+    console.log(accessRight);
+    console.log(result.rows);
+    await connection.close();
+    res.send(result.rows.map(e1=>e1.LOW))
+    return result.rows;
+}
+
+async function getAccessToken(res) {
+    const url = 'https://login.microsoftonline.com/0ae51e19-07c8-4e4b-bb6d-648ee58410f4/oauth2/v2.0/token';
+    
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    // Steffens
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("client_id", "4d8f6526-df32-4870-8afb-9937ba66d739");
+    urlencoded.append("scope", "api://dia-brain/.default");
+    urlencoded.append("client_secret", client_secret);
+    urlencoded.append("grant_type", "client_credentials");
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow'
+    };
+      
+
+    try {
+        const response = await fetch(url, requestOptions);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        // Assuming the JWT token is returned in the 'token' field
+        const token = data.access_token;
+        res.send(data)
+
+        // Optionally, you can store the token in localStorage or sessionStorage
+        //localStorage.setItem('jwtToken', token);
+
+        return token;
+    } catch (error) {
+        console.error('Authentication failed:', error);
+        throw error; // Rethrow the error for further handling
+    }
+}
+// end my code
 
 app.get('*', (req, res) => {
   res.status(404).send("Not Found");
